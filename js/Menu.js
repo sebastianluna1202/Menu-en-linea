7f0de27d542d376ed26e082463abba3699d38792
sebastianluna1202 <sebastian_luna1202@outlook.es>
2021-04-21 00:00:34 -0500

    Avance del menu

--- js/Menu.js
+++ js/Menu.js
@@ -1,182 +1,182 @@
 let platosDelMenu = [
     plato1 = {
         plato: "Corte de carne", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "almuerzo", 
         precio: "$25",
         nombreArchivo: "1.jpg",
     },
     plato2 = {
         plato: "Ensalada de camarones", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "almuerzo", 
         precio: "$25",
         nombreArchivo: "2.jpg",
     },
     plato3 = {
         plato: "Ensalada de pollo", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "almuerzo", 
         precio: "$25",
         nombreArchivo: "3.jpg",
     },
     plato4 = {
         plato: "Pasta", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "almuerzo", 
         precio: "$25",
         nombreArchivo: "4.jpg",
     },
     plato5 = {
-        plato: "Hamburguesa con papas", 
-        descripcion: 2,
+        plato: "Hamburguesa", 
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "almuerzo", 
         precio: "$25",
         nombreArchivo: "5.jpg",
     },
     plato6 = {
         plato: "Lasaña", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "almuerzo", 
         precio: "$25",
         nombreArchivo: "6.jpg",
     },
     plato7 = {
         plato: "Mole", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "almuerzo", 
         precio: "$25",
         nombreArchivo: "7.jpg",
     },
     plato8 = {
         plato: "Pizza gourmet", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "almuerzo", 
         precio: "$25",
         nombreArchivo: "8.jpg",
     },
     plato9 = {
         plato: "Alitas de pollo", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "almuerzo", 
         precio: "$25",
         nombreArchivo: "9.jpg",
     },
     plato10 = {
         plato: "Tacos", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "almuerzo", 
         precio: "$25",
         nombreArchivo: "10.jpg",
     },
     plato11 = {
         plato: "Cafe negro", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "desayuno", 
         precio: "$25",
         nombreArchivo: "11.jpg",
     },
     plato12 = {
         plato: "Capuccino", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "desayuno", 
         precio: "$25",
         nombreArchivo: "12.jpg",
     },
     plato13 = {
         plato: "Chilaquiles", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "desayuno", 
         precio: "$25",
         nombreArchivo: "13.jpg",
     },
     plato14 = {
         plato: "Club sandwich", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "desayuno", 
         precio: "$25",
         nombreArchivo: "14.jpg",
     },
     plato15 = {
         plato: "Croaissant", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "desayuno", 
         precio: "$25",
         nombreArchivo: "15.jpg",
     },
     plato16 = {
         plato: "Malteada de chocolate", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "desayuno", 
         precio: "$25",
         nombreArchivo: "16.jpg",
     },
     plato17 = {
-        plato: "Club sandwich", 
-        descripcion: 2,
+        plato: "Omelet", 
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "desayuno", 
         precio: "$25",
-        nombreArchivo: "17.jpg",
+        nombreArchivo: "17.png",
     },
     plato18 = {
         plato: "Panqueques", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "desayuno", 
         precio: "$25",
         nombreArchivo: "18.jpg",
     },
     plato19 = {
         plato: "Chocoflan", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "postre", 
         precio: "$25",
         nombreArchivo: "19.jpg",
     },
     plato20 = {
         plato: "Ensalada de crema", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "postre", 
         precio: "$25",
         nombreArchivo: "20.jpg",
     },
     plato21 = {
         plato: "Galleta", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "postre", 
         precio: "$25",
         nombreArchivo: "21.jpg",
     },
     plato22 = {
         plato: "Gelatina", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "postre", 
         precio: "$25",
         nombreArchivo: "22.jpg",
     },
     plato23 = {
         plato: "Helado", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "postre", 
         precio: "$25",
         nombreArchivo: "23.jpg",
     },
     plato24 = {
         plato: "Macarons", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "postre", 
         precio: "$25",
         nombreArchivo: "24.jpg",
     },
     plato25 = {
         plato: "Pastel de chocolate", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "postre", 
         precio: "$25",
         nombreArchivo: "25.webp",
     },
     plato26 = {
         plato: "Pay de queso", 
-        descripcion: 2,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
         tipo: "postre", 
         precio: "$25",
         nombreArchivo: "26.jpg",
@@ -187,30 +187,30 @@ menuAlmuerzo = platosDelMenu.filter( plato => plato.tipo == "almuerzo")
 menuDesayuno = platosDelMenu.filter( plato => plato.tipo == "desayuno")
 menuPostre = platosDelMenu.filter( plato => plato.tipo == "postre")
 
-function constructorDePlato(ruta, nombre, descripcion, precio) {
+function constructorDePlato(plato) {
     elemento = `
     <div class="container_plato" >
-        <img src="./assets/Imagenes/menu/${ruta.nombreArchivo}">
-        <h2>${nombre.plato}</h2>
-        <p>${descripcion.descripcion}</p>
-        <h3>${precio.precio}</h3>
+        <img src="./assets/Imagenes/menu/${plato.nombreArchivo}">
+        <div class="plato_description">
+            <h2>${plato.plato}</h2>
+            <p>${plato.descripcion}</p>
+            <h3>${plato.precio}</h3>
+        </div>
     </div> 
     `
-    return elemento
-}
-function constructorDeCodigo(menu) {
-    let plato 
-    let container = []
-    for ( i = 0 ; i > menu.length ; i++ ) {
-        plato = constructorDePlato(menu[i], menu[i], menu[i], menu[i])
-        container.unshift(plato)
-    }
-    let codigo = container.join("") //se puede usar unshift tambien
-    return codigo
+    section2.innerHTML += elemento
 }
 
-let imprimir= constructorDeCodigo(menuDesayuno)
-console.log(imprimir)
+
+function construirMenu(menu) {
+    section2.innerHTML = ""
+    menu.forEach( plato => {
+        constructorDePlato(plato)
+    })
+}
+construirMenu(platosDelMenu)
+
+
 
 
 
