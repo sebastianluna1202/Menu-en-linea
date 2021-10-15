c6e0e42321af1df07c48cea480fbf4fe95104160
sebastianluna1202 <sebastian_luna1202@outlook.es>
2021-10-14 23:51:59 -0500

    Update

--- js/desplegar_menu.js
+++ js/desplegar_menu.js
@@ -1,18 +1,33 @@
+const menuIcon = document.querySelector('.menu-icon')
+const firstSpan = document.querySelector('.first-span')
+const seccondSpan = document.querySelector('.seccond-span')
+const thirdSpan = document.querySelector('.third-span')
+const nav = document.querySelector('.nav')
 
-let iconoMenu = document.getElementById("iconoDeMenu");
-let estadoMenu = true;
-iconoMenu.addEventListener('click', (e) => {
-    e.preventDefault()
-    let menu = document.getElementById("menuDesplegable");
-    if (estadoMenu) {
-        menu = menu.classList.remove("hidden");
-        estadoMenu = false;
+
+document.addEventListener('click', (event) => {
+  console.log(event)
+  if (
+    event.target.classList.contains('menu-icon') || 
+    event.target.classList.contains('first-span') ||
+    event.target.classList.contains('seccond-span') ||
+    event.target.classList.contains('third-span')
+  ) {
+    if (menuIcon.classList.contains('active') != true) {
+      menuIcon.classList.add('active')
+      nav.classList.remove('display-none')
+      setTimeout(() => {nav.classList.remove('hidden')}, 0)
     } else {
-       menu = menu.classList.add("hidden");
-       estadoMenu = true;
+      menuIcon.classList.remove('active')
+      nav.classList.add('hidden')
+      setTimeout(() => {nav.classList.add('display-none')}, 300)
     }
-});
-
+  } else {
+    menuIcon.classList.remove('active')
+    nav.classList.add('hidden')
+    setTimeout(() => {nav.classList.add('display-none')}, 300)
+  }
+})
 
 
 
--- js/filtrar_platos.js
+++ js/filtrar_platos.js
@@ -1,186 +1,186 @@
 let platosDelMenu = [
-    plato1 = {
-        plato: "Corte de carne", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "almuerzo", 
-        precio: "$25",
-        nombreArchivo: "1.jpg",
-    },
-    plato2 = {
-        plato: "Ensalada de camarones", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "almuerzo", 
-        precio: "$25",
-        nombreArchivo: "2.jpg",
-    },
-    plato3 = {
-        plato: "Ensalada de pollo", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "almuerzo", 
-        precio: "$25",
-        nombreArchivo: "3.jpg",
-    },
-    plato4 = {
-        plato: "Pasta", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "almuerzo", 
-        precio: "$25",
-        nombreArchivo: "4.jpg",
-    },
-    plato5 = {
-        plato: "Hamburguesa", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "almuerzo", 
-        precio: "$25",
-        nombreArchivo: "5.jpg",
-    },
-    plato6 = {
-        plato: "Lasaña", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "almuerzo", 
-        precio: "$25",
-        nombreArchivo: "6.jpg",
-    },
-    plato7 = {
-        plato: "Mole", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "almuerzo", 
-        precio: "$25",
-        nombreArchivo: "7.jpg",
-    },
-    plato8 = {
-        plato: "Pizza gourmet", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "almuerzo", 
-        precio: "$25",
-        nombreArchivo: "8.jpg",
-    },
-    plato9 = {
-        plato: "Alitas de pollo", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "almuerzo", 
-        precio: "$25",
-        nombreArchivo: "9.jpg",
-    },
-    plato10 = {
-        plato: "Tacos", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "almuerzo", 
-        precio: "$25",
-        nombreArchivo: "10.jpg",
-    },
-    plato11 = {
-        plato: "Cafe negro", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "desayuno", 
-        precio: "$25",
-        nombreArchivo: "11.jpg",
-    },
-    plato12 = {
-        plato: "Capuccino", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "desayuno", 
-        precio: "$25",
-        nombreArchivo: "12.jpg",
-    },
-    plato13 = {
-        plato: "Chilaquiles", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "desayuno", 
-        precio: "$25",
-        nombreArchivo: "13.jpg",
-    },
-    plato14 = {
-        plato: "Club sandwich", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "desayuno", 
-        precio: "$25",
-        nombreArchivo: "14.jpg",
-    },
-    plato15 = {
-        plato: "Croaissant", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "desayuno", 
-        precio: "$25",
-        nombreArchivo: "15.jpg",
-    },
-    plato16 = {
-        plato: "Malteada de chocolate", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "desayuno", 
-        precio: "$25",
-        nombreArchivo: "16.jpg",
-    },
-    plato17 = {
-        plato: "Omelet", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "desayuno", 
-        precio: "$25",
-        nombreArchivo: "17.png",
-    },
-    plato18 = {
-        plato: "Panqueques", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "desayuno", 
-        precio: "$25",
-        nombreArchivo: "18.jpg",
-    },
-    plato19 = {
-        plato: "Chocoflan", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "postre", 
-        precio: "$25",
-        nombreArchivo: "19.jpg",
-    },
-    plato20 = {
-        plato: "Ensalada de crema", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "postre", 
-        precio: "$25",
-        nombreArchivo: "20.jpg",
-    },
-    plato21 = {
-        plato: "Galleta", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "postre", 
-        precio: "$25",
-        nombreArchivo: "21.jpg",
-    },
-    plato22 = {
-        plato: "Gelatina", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "postre", 
-        precio: "$25",
-        nombreArchivo: "22.jpg",
-    },
-    plato23 = {
-        plato: "Helado", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "postre", 
-        precio: "$25",
-        nombreArchivo: "23.jpg",
-    },
-    plato24 = {
-        plato: "Macarons", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "postre", 
-        precio: "$25",
-        nombreArchivo: "24.jpg",
-    },
-    plato25 = {
-        plato: "Pastel de chocolate", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "postre", 
-        precio: "$25",
-        nombreArchivo: "25.webp",
-    },
-    plato26 = {
-        plato: "Pay de queso", 
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
-        tipo: "postre", 
-        precio: "$25",
-        nombreArchivo: "26.jpg",
-    }
+  plato1 = {
+    plato: "Corte de carne",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "almuerzo",
+    precio: "$25",
+    nombreArchivo: "1.jpg",
+  },
+  plato2 = {
+    plato: "Ensalada de camarones",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "almuerzo",
+    precio: "$25",
+    nombreArchivo: "2.jpg",
+  },
+  plato3 = {
+    plato: "Ensalada de pollo",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "almuerzo",
+    precio: "$25",
+    nombreArchivo: "3.jpg",
+  },
+  plato4 = {
+    plato: "Pasta",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "almuerzo",
+    precio: "$25",
+    nombreArchivo: "4.jpg",
+  },
+  plato5 = {
+    plato: "Hamburguesa",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "almuerzo",
+    precio: "$25",
+    nombreArchivo: "5.jpg",
+  },
+  plato6 = {
+    plato: "Lasaña",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "almuerzo",
+    precio: "$25",
+    nombreArchivo: "6.jpg",
+  },
+  plato7 = {
+    plato: "Mole",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "almuerzo",
+    precio: "$25",
+    nombreArchivo: "7.jpg",
+  },
+  plato8 = {
+    plato: "Pizza gourmet",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "almuerzo",
+    precio: "$25",
+    nombreArchivo: "8.jpg",
+  },
+  plato9 = {
+    plato: "Alitas de pollo",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "almuerzo",
+    precio: "$25",
+    nombreArchivo: "9.jpg",
+  },
+  plato10 = {
+    plato: "Tacos",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "almuerzo",
+    precio: "$25",
+    nombreArchivo: "10.jpg",
+  },
+  plato11 = {
+    plato: "Cafe negro",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "desayuno",
+    precio: "$25",
+    nombreArchivo: "11.jpg",
+  },
+  plato12 = {
+    plato: "Capuccino",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "desayuno",
+    precio: "$25",
+    nombreArchivo: "12.jpg",
+  },
+  plato13 = {
+    plato: "Chilaquiles",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "desayuno",
+    precio: "$25",
+    nombreArchivo: "13.jpg",
+  },
+  plato14 = {
+    plato: "Club sandwich",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "desayuno",
+    precio: "$25",
+    nombreArchivo: "14.jpg",
+  },
+  plato15 = {
+    plato: "Croaissant",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "desayuno",
+    precio: "$25",
+    nombreArchivo: "15.jpg",
+  },
+  plato16 = {
+    plato: "Malteada de chocolate",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "desayuno",
+    precio: "$25",
+    nombreArchivo: "16.jpg",
+  },
+  plato17 = {
+    plato: "Omelet",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "desayuno",
+    precio: "$25",
+    nombreArchivo: "17.png",
+  },
+  plato18 = {
+    plato: "Panqueques",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "desayuno",
+    precio: "$25",
+    nombreArchivo: "18.jpg",
+  },
+  plato19 = {
+    plato: "Chocoflan",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "postre",
+    precio: "$25",
+    nombreArchivo: "19.jpg",
+  },
+  plato20 = {
+    plato: "Ensalada de crema",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "postre",
+    precio: "$25",
+    nombreArchivo: "20.jpg",
+  },
+  plato21 = {
+    plato: "Galleta",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "postre",
+    precio: "$25",
+    nombreArchivo: "21.jpg",
+  },
+  plato22 = {
+    plato: "Gelatina",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "postre",
+    precio: "$25",
+    nombreArchivo: "22.jpg",
+  },
+  plato23 = {
+    plato: "Helado",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "postre",
+    precio: "$25",
+    nombreArchivo: "23.jpg",
+  },
+  plato24 = {
+    plato: "Macarons",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "postre",
+    precio: "$25",
+    nombreArchivo: "24.jpg",
+  },
+  plato25 = {
+    plato: "Pastel de chocolate",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "postre",
+    precio: "$25",
+    nombreArchivo: "25.webp",
+  },
+  plato26 = {
+    plato: "Pay de queso",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quasi dolores, nemo deleniti voluptatum quas?",
+    tipo: "postre",
+    precio: "$25",
+    nombreArchivo: "26.jpg",
+  }
 ]
 
 section2 = document.getElementById("section-2")
@@ -189,51 +189,79 @@ menuDesayuno = platosDelMenu.filter( plato => plato.tipo == "desayuno")
 menuPostre = platosDelMenu.filter( plato => plato.tipo == "postre")
 
 function constructorDePlato(plato) {
-    elemento = `
-    <div class="container_plato" >
-        <img src="./assets/Imagenes/menu/${plato.nombreArchivo}">
-        <div class="plato_description">
-            <h2>${plato.plato}</h2>
-            <p>${plato.descripcion}</p>
-            <h3>${plato.precio}</h3>
-        </div>
-    </div> 
-    `
-    section2.innerHTML += elemento
+  elemento = `
+  <div class="container_plato" >
+    <img src="./assets/images/menu/${plato.nombreArchivo}">
+    <div class="plato_description">
+      <h2>${plato.plato}</h2>
+      <p class="raiting">★★★★★</p>
+      <p>${plato.descripcion}</p>
+      <h3>${plato.precio}</h3>
+    </div>
+  </div>
+  `
+  return elemento
 }
 
 function construirMenu(menu) {
-    section2.innerHTML = ""
-    menu.forEach( plato => {
-        constructorDePlato(plato)
-    })
-}
+  section2.innerHTML = ""
+  let platos = []
+  menu.forEach( plato => {
+    platos.push(constructorDePlato(plato))
+  })
+  platos.forEach(plato => {
+    section2.innerHTML += plato
+  })
+  const platosNodeList = document.querySelectorAll('.container_plato')
+  const platosDelDom = [...platosNodeList]
+  const platosPromesas = [] 
 
+  platosDelDom.forEach( plato => {
+    const printDish = () => {
+      return new Promise ((resolve, reject) => {
+        true
+        ? setTimeout(() => {resolve(plato)}, 75)
+        : reject(new Error)
+      })
+    }
+    platosPromesas.push(printDish)
+  })
+  const printDishAsync = async () => {
+    for (let i = 0; i < platosPromesas.length; i++) {
+      await platosPromesas[i]().then(plato => { plato.classList.add('appear')})
+    }
+  }
+  setTimeout(() => {
+    printDishAsync()
+    window.scroll(0,0)
+  }, 500)  
+  
+}
 construirMenu(platosDelMenu)
 
-botonMenuCompleto = document.getElementById("menu_completo")
-botonMenuCompleto.addEventListener('click', (e) => {
-    e.preventDefault()
-    construirMenu(platosDelMenu)
-})
+// botonMenuCompleto = document.getElementById("menu_completo")
+// botonMenuCompleto.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   construirMenu(platosDelMenu)
+// })
 
-botonMenuDesayuno = document.getElementById("menu_desayuno")
-botonMenuDesayuno.addEventListener('click', (e) => {
-    e.preventDefault()
-    construirMenu(menuDesayuno)
-})
+// botonMenuDesayuno = document.getElementById("menu_desayuno")
+// botonMenuDesayuno.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   construirMenu(menuDesayuno)
+// })
 
-botonMenuPostre = document.getElementById("menu_postre")
-botonMenuPostre.addEventListener('click', (e) => {
-    e.preventDefault()
-    construirMenu(menuPostre)
-})
+// botonMenuPostre = document.getElementById("menu_postre")
+// botonMenuPostre.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   construirMenu(menuPostre)
+// })
 
-botonMenuAlmuerzo = document.getElementById("menu_almuerzo")
-botonMenuAlmuerzo.addEventListener('click', (e) => {
-    e.preventDefault()
-    construirMenu(menuAlmuerzo)
-})
+// botonMenuAlmuerzo = document.getElementById("menu_almuerzo")
+// botonMenuAlmuerzo.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   construirMenu(menuAlmuerzo)
+// })
 
 
 
